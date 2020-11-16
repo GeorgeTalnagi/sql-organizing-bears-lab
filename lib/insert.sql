b824865a5eed55cef55db7b81349d62ad4f3d0c9
georgetalnagi <gtalnagi@yahoo.com>
2020-11-16 18:20:53 -0500

    commit tree 19aced57db7118851ab83794570f5b4f473a995d

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (1, "Paul", 4, "male", "brown", "friendly", true); 
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (2, "Kacy", 4, "female", "black", "angry", true); 
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (3, "Jim", 4, "male", "brown", "friendly", true); 
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (4, "Bob", 4, "male", "white", "protective", true); 
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (5, "Kristen", 4, "female", "brown", "friendly", false); 
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (6, "Alice", 4, "female", "brown", "shy", true); 
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (7, "Jake", 4, "male", "black", "friendly", false); 
+INSERT INTO bears (id, age, sex, color, temperament, alive) VALUES (8, 4, "female", "brown", "friendly", true); 
